f79cc4af316f0c25f2f5a374a12e7fb02d628b59
Ethan <kkojaeh@gmail.com>
2019-02-23 09:54:03 +0900

    ddl 생성

--- impl/src/main/resources/fax/flyway/mysql/V1__initialize.sql
+++ impl/src/main/resources/fax/flyway/mysql/V1__initialize.sql
@@ -1,30 +1,14 @@
-create table prr_fax (
+create table fax_fax (
 	id binary(16) not null,
-	accepted_date datetime,
-	accepter_id varchar(50),
-	asap bit not null,
-	canceled_date datetime,
-	canceler_id varchar(50),
-	code varchar(20),
-	committed_date datetime,
-	committer_id varchar(50),
-	completed_date datetime,
-	created_by_id varchar(50),
-	created_by_name varchar(50),
-	created_date datetime,
-	due_date datetime,
-	item_id binary(16),
-	last_modified_by_id varchar(50),
-	last_modified_by_name varchar(50),
-	last_modified_date datetime,
-	plan_id binary(16),
-	project_id binary(16),
-	quantity decimal(19,2),
+	description varchar(200),
+	executed_id varchar(100),
+	executed_count integer not null,
+	executed_date datetime,
+	requested_date datetime,
 	requester_id varchar(50),
-	spare_quantity decimal(19,2),
-	status varchar(20),
+	terminated bit not null,
 	primary key (id)
 ) engine=InnoDB;
 
-create index IDX9oab398ibuwdr5dvv0k3kh95b
-	on prr_fax (plan_id);
+create index IDXfw228votptbut42y6qgp9eil7
+	on fax_fax (requested_date);
